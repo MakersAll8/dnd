1d66f468ee05fef46dd41ec6dde598e40f7079e8
yanceyy <yanceyy@users.noreply.github.com>
2022-05-03 09:57:14 +1000

    test: add testings for sortInTwoColumn sortInOneColumn

--- src/__tests__/utils.test.tsx
+++ src/__tests__/utils.test.tsx
@@ -1,4 +1,4 @@
-import {deepCopyWidgets, isOverFlow} from '../utils/utils';
+import {deepCopyWidgets, isOverFlow, sortInOneColumn, sortInTwoColumn} from '../utils/utils';
 
 import type { Widgets } from "../state";
 
@@ -21,11 +21,35 @@ describe('testing utils function', () => {
     expect(copiedWidgets).not.toBe(Widgets);
     })
 
-    it('copied children attribute should be the same object',()=>{
+    it('copied children attribute should be the same object after copy',()=>{
       const copiedWidgets = deepCopyWidgets(Widgets);
       const firstChild = Widgets[0].children;
       const CopiedFirstChild = copiedWidgets[0].children;
       expect(CopiedFirstChild).toBe(firstChild);
       })
+  })
+
+  describe('sortInOneColumn',()=>{
+    const Widgets=[
+      {top:0,left:1,width:3,height:1,name:'a',children:()=>(<></>)},
+      {top:0,left:2,width:1,height:1,name:'b',children:()=>(<></>)},
+    ] as Widgets;
+    it('left and top value become expected',()=>{
+      const sortedWidgets = sortInOneColumn(Widgets);
+      expect(sortedWidgets[0]).toMatchObject({left:0,top:0});
+      expect(sortedWidgets[1]).toMatchObject({left:0,top:2});
+    })
+  })
+
+  describe('sortInTwoColumn',()=>{
+    const Widgets=[
+      {top:0,left:1,width:3,height:1,name:'a',children:()=>(<></>)},
+      {top:0,left:3,width:1,height:1,name:'b',children:()=>(<></>)},
+    ] as Widgets;
+    it('left and top value become expected',()=>{
+      const sortedWidgets = sortInTwoColumn(Widgets);
+      expect(sortedWidgets[0]).toMatchObject({left:1,top:0});
+      expect(sortedWidgets[1]).toMatchObject({left:0,top:0});
+    })
   })
 })
